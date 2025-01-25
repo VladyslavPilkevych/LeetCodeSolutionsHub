e0bd0136eefe3097dee8791fe11fb14065d31386
VladyslavPilkevych <vladyslavpilkevych2004@gmail.com>
2025-01-25 17:49:19 +0100

    topics/strings Duplicate Zeros - kotlin

---
+++ Topics/Arrays_101/02_Duplicate_Zeros/Solution.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun duplicateZeros(arr: IntArray): Unit {
+        var zeroes = 0
+
+        for (i in arr) {
+            if (i == 0) {
+                zeroes++
+            }
+        }
+
+        var i = arr.size - 1
+        var j = arr.size + zeroes - 1
+
+        while (i != j) {
+            insert(arr, i, j--)
+            if (arr[i] == 0) {
+                insert(arr, i, j--)
+            }
+            i--
+        }
+    }
+
+    private fun insert(arr: IntArray, i: Int, j: Int) {
+        if (j < arr.size) {
+            arr[j] = arr[i]
+        }
+    }
+}
